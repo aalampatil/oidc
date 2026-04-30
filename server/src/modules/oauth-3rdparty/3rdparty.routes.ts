2ef362d6778f6d993beeca95026db983ddcb6b27
aalampatil <aalam.workacc@gmail.com>
2026-04-30 16:04:44 +0530

    check

--- server/src/modules/oauth-3rdparty/3rdparty.routes.ts
+++ server/src/modules/oauth-3rdparty/3rdparty.routes.ts
@@ -1,5 +1,4 @@
 import crypto from "node:crypto";
-import path from "node:path";
 import { eq } from "drizzle-orm";
 import { db } from "../../db/index";
 import { authCodesTable, oauthClientsTable, usersTable } from "../../db/schema";
